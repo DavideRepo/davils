84956bec667197e1b91b45e70ae44a21996d5dcc
davide raviolo <davide.raviolo@outlook.it>
2023-10-18 12:07:56 +0200

    first commit

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/DavideRepo/davils",
     packages=setuptools.find_packages(),
-    install_requires=['numpy', 'scipy', 'matplotlib', 'pickle', 'h5py'],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'h5py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
